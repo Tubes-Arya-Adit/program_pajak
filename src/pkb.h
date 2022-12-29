c7983a63eba0ae4f88b7ab68502796c8ce62c800
adityamahendrap <ptadityamahendrap@gmail.com>
2022-12-29 19:28:27 +0800

    Fixing Bug

--- src/pkb.h
+++ src/pkb.h
@@ -1,5 +1,3 @@
-#define admin 0
-
 int jenis_kendaraan, tahunan_pajak, kode_ranmor, is_thn_pertama, kepemilikan_ke, menu_pkb, lihat_rincian, thn_bayar, bln_bayar, tgl_bayar;
 float njkb,    // nilai jual kendaraan bermotor
     bbnkb = 0, // Bea Balik Nama Kendaraan Bermotor (BBN KB)
@@ -9,9 +7,11 @@ float njkb,    // nilai jual kendaraan bermotor
     pnbp_tnkb,
     pengesahan_stnk,
     koefisien,
-    total;
+    total, 
+    admin = 50000;
 char merk[100], tipe[100], str_jenis_kendaraan[100], str_kode_ranmor[100], stnk[100], str_tahunan_pajak[100], status[100];
 
+void output_pkb();
 void pkb_menu();
 void motor();
 void mobil();
@@ -52,7 +52,7 @@ void output_pkb()
     fprintf(file, "\n\t Tipe Kendaraan  : %s", tipe);
     fprintf(file, "\n\t Merk            : %s", merk);
     fprintf(file, "\n\t Kode Kendaraan  : %s", str_kode_ranmor);
-    fprintf(file, "\n\t Kepemilikan Ke  : %s", kepemilikan_ke);
+    fprintf(file, "\n\t Kepemilikan Ke  : %d", kepemilikan_ke);
     fprintf(file, "\n\t Jenis Pajak     : %s", str_tahunan_pajak);
     fprintf(file, "\n\t-------------------------------------------------------------");
     fprintf(file, "\n");
@@ -75,7 +75,7 @@ void output_pkb()
     fprintf(file, "\n\tD. RINCIAN TRANSAKSI");
     fprintf(file, "\n\t-------------------------------------------------------------");
     fprintf(file, "\n\t Tahun Pajak     : %02d", waktu_sekarang.tahun);
-    fprintf(file, "\n\t Tenggat         : ");
+    fprintf(file, "\n\t Tenggat         : 31 Desember");
     fprintf(file, "\n\t Waktu Transaksi : %d-%02d-%02d %02d:%02d:%02d", waktu_sekarang.hari, waktu_sekarang.bulan, waktu_sekarang.tahun, waktu_sekarang.jam, waktu_sekarang.menit, waktu_sekarang.detik);
     fprintf(file, "\n\t Status          : %s", status);
     fprintf(file, "\n\t-------------------------------------------------------------");
